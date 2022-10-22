9e0d6411f01db9c05ed1f5c687ce9d03eefde8d9
Hiroto-Nagashima <nh35813219254@gmail.com>
2022-10-23 00:46:48 +0900

    BasicPriceのrateメソッド適用

--- ruby/policy_object/after.rb
+++ ruby/policy_object/after.rb
@@ -41,15 +41,15 @@ class ShippingFee
   def initialize(destination, total_number_of_items)
     @destination = destination
     @total_number_of_items = total_number_of_items
-    @basic_price = 100
+    @basic_price = BasicPrice.new(100)
   end
 
   def calculate_fee
     policy = ShippingFeePolicy.new(@destination, @total_number_of_items)
 
-    @basic_price * 1.5 if policy.deliver_to_japan_with_special_rate?
-    @basic_price * 2.0 if policy.deliver_abroad_with_special_rate?
-    @basic_price * 1.0
+    @basic_price.rate(1.5) if policy.deliver_to_japan_with_special_rate?
+    @basic_price.rate(2.0) if policy.deliver_abroad_with_special_rate?
+    @basic_price.rate(1.0)
   end
 end
 
